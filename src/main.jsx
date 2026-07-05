c4e990386322db3fc464088d2f1c88a8a9a7d4a3
brijithm <brijithmshibu38@gmail.com>
2026-07-05 11:18:40 +0530

    add basename to browserrouter

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/updo-frontend/">
       <App />
     </BrowserRouter>
   </StrictMode>,
